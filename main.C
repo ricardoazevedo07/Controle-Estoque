8e7d15ed9f912358c2ab31941b9331cab9e2212b
ricardoazevedo07 <rick.azevedo07@gmail.com>
2017-05-01 00:31:34 -0300

    Add files via upload

---
+++ main.C
@@ -0,0 +1,26 @@
+/*programa: controle de estoque
+
+Matéria: ESTRUTURA DE DADOS EM C
+Assunto:
+
+Obs**
+
+CONTROLE DE ESTOQUE
+SO utilizado: arch linux x64
+versão do compilador:
+
+*/
+
+#include "power.h"
+
+
+
+int main(){
+
+    menu();
+
+
+        //scanf("%*c");
+        //system("exit");
+  return 0;
+}
